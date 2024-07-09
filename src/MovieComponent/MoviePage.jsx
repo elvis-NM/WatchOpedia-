92d04eaa3f3eb861a5fb4c1bbe2b4c640e869bd6
Elvis Makia <elvis_makia@yahoo.com>
2024-07-08 23:39:03 -0400

    AddMovie list  commit

--- src/MovieComponent/MoviePage.jsx
+++ src/MovieComponent/MoviePage.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import AddMovie from "./AddMovie";
+import MovieList from "./MovieList";
 //movieList: ["Die Hard", "Harry Potter"]
 const MoviePage = () => {
+  const [movieState, setMovieState] = useState(() => {
+    //set state
+    return { movieList: ["Die Hard", "Harry Potter"] };
+  });
+
   return (
     <div className="container col-12 col-md-6 my-3 border">
       <AddMovie />
+      <MovieList movieList={movieState.movieList} />
     </div>
   );
 };
