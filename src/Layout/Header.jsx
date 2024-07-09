8e4f0eeec5949f07fb5918811b18752f5a2deaf4
Elvis Makia <elvis_makia@yahoo.com>
2024-07-08 22:06:59 -0400

    added hooks in counter function  commit

---
+++ src/Layout/Header.jsx
@@ -0,0 +1,13 @@
+import logo from "../Images/react.png";
+
+const Header = () => {
+  return (
+    <div className="py-2 p1-2" style={{ borderBottom: "1px solid #777" }}>
+      <img src={logo} alt="" style={{ height: "35px", verticalAlign: "top" }} />
+
+      <span className="h2 pt-4 m-2 text=white-100">WatchOpedia</span>
+    </div>
+  );
+};
+
+export default Header;
